2208f21064b704271eb81728e50da840f0d165b6
Alex Westerman <westeraj@rose-hulman.edu>
2022-10-17 20:20:38 -0400

    Add EditorConfig

--- RetireSimple.Backend/Program.cs
+++ RetireSimple.Backend/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -7,11 +8,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
--- RetireSimple.Backend/WeatherForecast.cs
+++ RetireSimple.Backend/WeatherForecast.cs
@@ -1,7 +1,5 @@
-namespace RetireSimple.Backend
-{
-    public class WeatherForecast
-    {
+namespace RetireSimple.Backend {
+    public class WeatherForecast {
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
