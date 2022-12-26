003a29bb6ba674c2ebd882daa67b345b075859a8
Alex Westerman <westeraj@rose-hulman.edu>
2022-12-26 11:32:43 -0500

    Scuffed Monte Carlo Implementation (I think?)

---
+++ RetireSimple.Backend/DomainModel/Analysis/MonteCarlo.cs
@@ -0,0 +1,90 @@
+﻿using MathNet.Numerics.Distributions;
+using RetireSimple.Backend.DomainModel.Data;
+
+namespace RetireSimple.Backend.DomainModel.Analysis {
+	public class MonteCarlo {
+
+		public static List<(string, decimal)> MonteCarloSim_LogNormal(int numSteps, decimal startVal, Dictionary<string, string> simOptions) {
+			var model = new List<(string, decimal)>();
+			var step = 0;
+			var mu = double.Parse(simOptions["mu"]);
+			var sigma = double.Parse(simOptions["sigma"]);
+
+			var distribution = new LogNormal(mu, sigma);
+
+			model.Add((step.ToString(), startVal));
+
+
+			return model;
+		}
+
+
+		//NOTE I think this is wrong, purely scratch work
+		//public static List<(string, decimal)> MonteCarloSim_GeometricBrownianMotion(int numSteps, decimal startVal, Dictionary<string, string> simOptions) {
+		//	var model = new List<(string, decimal)>();
+		//	var step = 0;
+
+		//	var stockMu = double.Parse(simOptions["stockMu"]);
+		//	var stockSigma = double.Parse(simOptions["stockSigma"]);
+		//	var randomVarMu = double.Parse(simOptions["randomVarMu"]);
+		//	var randomVarSigma = double.Parse(simOptions["randomVarSigma"]);
+		//	var stockPrice = double.Parse(simOptions["stockPrice"]);
+
+		//	//NOTE Can't use lognormal as that distribution can only be positive
+		//	var randomVar = new Normal(randomVarMu, randomVarSigma);
+
+
+		//	model.Add((step.ToString(), startVal));
+
+		//	while(step < numSteps) {
+		//		var drift = stockMu * step;
+		//		var shock = stockSigma * randomVar.Sample() * Math.Sqrt(step);
+		//		var baseDelta = drift + shock;
+		//		//var scaledDelta = stockPrice * baseDelta; 
+
+		//		//var modelVal = stockPrice + scaledDelta;
+
+		//		model.Add((step.ToString(), (decimal)(stockPrice + baseDelta)));
+
+		//		step++;
+		//	}
+
+
+		//	return model;
+		//}
+
+		public static List<(string, decimal)> MonteCarloSim_GeometricBrownianMotion(int numSteps, double startVal, double mu, double sigma) {
+			var model = new List<(string, decimal)>();
+			var step = 0;
+			var BrownianMotion = new Normal(mu, sigma);
+
+				
+			while(step < numSteps) {
+				var exp_fact = (mu - (sigma * sigma) / 2) * step;
+				exp_fact += sigma * BrownianMotion.Sample();
+				var modelVal = startVal * Math.Exp(exp_fact);
+				model.Add((step.ToString(), (decimal)modelVal));
+				step++;
+			}
+
+			return model;
+		}
+
+
+		//TODO encode this into a reasonable string and not steps!
+
+		////Encodes periods to MMYYYY
+		//public static string MakePeriodString(int month, int year) {
+		//	return month.ToString("00") + year.ToString();
+		//}
+
+		//public static string ConvertStepToPeriod(int step) {
+		//	var baseMonth = DateTime.Today.
+
+
+		//}
+
+
+
+	}
+}
---
+++ RetireSimple.Backend/DomainModel/MonteCarloTest.csx
@@ -0,0 +1,41 @@
+﻿#r "D:\Onedrive\SeniorProject\RetireSimple\RetireSimple.Backend\bin\Debug\net6.0\RetireSimple.Backend.dll"
+#r "D:\Onedrive\SeniorProject\RetireSimple\RetireSimple.Backend\bin\Debug\net6.0\MathNet.Numerics.dll"
+using System.Collections.Generic;
+
+using RetireSimple.Backend.DomainModel.Analysis;
+
+//var testDict = new Dictionary<string, string>();
+//testDict["stockMu"] = "0.5";
+//testDict["stockSigma"] = "0.05";
+//testDict["randomVarMu"] = "0";
+//testDict["randomVarSigma"] = "1";
+//testDict["stockPrice"] = "100";
+
+//var testModel = RetireSimple.Backend.DomainModel.Analysis.MonteCarlo.MonteCarloSim_GeometricBrownianMotion(100, (decimal) 100, testDict);
+
+var testAggregate = new List<List<(string, decimal)>>();
+for (int i = 0; i < 100; i++) {
+	testAggregate.Add(MonteCarlo.MonteCarloSim_GeometricBrownianMotion(100, 100.0, 0, 1));
+}
+
+
+var csvStrings = new List<string>();
+for (var i = 0; i < 100; i++) {
+	var stepRow = i.ToString();
+	foreach (var testModel in testAggregate) {
+		var modelStepValue = testModel.ElementAt(i).Item2;
+		stepRow += ("," + modelStepValue.ToString());
+	}
+	csvStrings.Add(stepRow);
+}
+
+
+File.WriteAllLines("D:\\Onedrive\\SeniorProject\\RetireSimple\\RetireSimple.Backend\\DomainModel\\MonteCarloTest.csv", csvStrings);
+//foreach (var csvString in csvStrings) {
+	//Console.WriteLine(csvString);
+	//File.AppendAllText("D:\\Onedrive\\SeniorProject\\RetireSimple\\RetireSimple.Backend\\DomainModel\\MonteCarloTest.csv", csvString + Environment.NewLine);
+//}
+
+//foreach (var step in testModel) {
+//		Console.WriteLine(step);
+//	}
