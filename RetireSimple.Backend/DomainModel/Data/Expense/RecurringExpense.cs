98547aa56779fb2149025fa131c2119154b8e3c7
Alex Westerman <westeraj@rose-hulman.edu>
2023-01-07 17:53:05 -0500

    Expense Object Documentation

--- RetireSimple.Backend/DomainModel/Data/Expense/RecurringExpense.cs
+++ RetireSimple.Backend/DomainModel/Data/Expense/RecurringExpense.cs
@@ -1,10 +1,22 @@
 ﻿namespace RetireSimple.Backend.DomainModel.Data.Expense {
+	/// <summary>
+	/// Represent a recurring expense on an investment
+	/// </summary>
 	public class RecurringExpense : ExpenseBase {
-		//How many months between each expense
-		//(e.g. 1 = monthly, 3 = quarterly, 12 = yearly)
+		/// <summary>
+		/// How many months between each expense
+		/// (e.g. 1 = monthly, 3 = quarterly, 12 = yearly)
+		/// </summary>
 		public int Frequency { get; set; }
 
+		/// <summary>
+		/// The date of the first recurrence
+		/// </summary>
 		public DateTime StartDate { get; set; }
+
+		/// <summary>
+		/// The date of the last recurrence
+		/// </summary>
 		public DateTime? EndDate { get; set; }
 
 		public override List<DateTime> GetExpenseDates() => throw new NotImplementedException();
