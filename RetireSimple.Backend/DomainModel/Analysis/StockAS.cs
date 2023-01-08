abf462a8621437e2db4bac2980ec8e288eafd2fd
Alex Westerman <westeraj@rose-hulman.edu>
2023-01-08 14:03:40 -0500

    Stock Dividend Logic

--- RetireSimple.Backend/DomainModel/Analysis/StockAS.cs
+++ RetireSimple.Backend/DomainModel/Analysis/StockAS.cs
@@ -1,4 +1,5 @@
-﻿using RetireSimple.Backend.DomainModel.Data;
+﻿using Microsoft.Extensions.Options;
+using RetireSimple.Backend.DomainModel.Data;
 using RetireSimple.Backend.DomainModel.Data.Investment;
 
 namespace RetireSimple.Backend.DomainModel.Analysis {
@@ -7,12 +8,58 @@ namespace RetireSimple.Backend.DomainModel.Analysis {
 
 		//This dictionary is statically available to allow for a common set of defaults that all
 		//analysis modules for the same type of investment can use. 
-		
+
 		public static readonly OptionsDict DefaultStockAnalysisOptions = new() {
-			["StockAnalysisExpectedGrowth"] = "0.1",	//Example Analysis Parameter
+			["AnalysisLength"] = "60",                          //Number of months to project
+			["StockAnalysisExpectedGrowth"] = "0.1",            //Expected Percentage Growth of the stock
+
 		};
 
+		#region "Dividend Calculations"
+		/// NOTE: These do not produce <see cref="InvestmentModel"/> objects. They are used in conjunction with an existing model 
+		/// in most cases to calculate the resulting stock value
 
+		private static int GetDividendIntervalMonths(string interval) => interval switch {
+			"Month" => 1,
+			"Quarter" => 3,
+			"Annual" => 12,
+			_ => throw new ArgumentException("Invalid Dividend Interval")
+		};
+
+		/// <summary>
+		/// Generates a list of monthly projected stock quantities using stock dividend distribution.
+		/// First element of list is assumed to be for the current month/year.
+		/// </summary>
+		/// <param name="investment"></param>
+		/// <param name="options"></param>
+		/// <returns></returns>
+		public static List<decimal> ProjectStockDividend(StockInvestment investment, OptionsDict options) {
+			var quantityList = new List<decimal>();
+			var stockQuantity = investment.StockQuantity;
+			var dividendPercent = investment.StockDividendPercent;
+			var currentMonth = DateTime.Now.Month;
+			var firstDividendMonth = investment.StockDividendFirstPaymentDate.Month;
+			var monthInterval = GetDividendIntervalMonths(investment.StockDividendDistributionInterval);
+
+			//quantityList.Add(stockQuantity);
+			for(int i = 0; i < int.Parse(options["AnalysisLength"]); i++) {
+				if((currentMonth - firstDividendMonth) % monthInterval == 0) {
+					stockQuantity += stockQuantity * dividendPercent;
+				}
+				quantityList.Add(stockQuantity);
+				currentMonth++;
+				if(currentMonth > 12) {
+					currentMonth = 1;
+				}
+			}
+
+			return quantityList;
+		}
+
+
+		#endregion
+
+		//TODO Move to Testing/Debugging
 		public static InvestmentModel testAnalysis(StockInvestment investment, OptionsDict options) {
 			var value = investment.StockPrice * investment.StockQuantity;
 
@@ -34,6 +81,7 @@ namespace RetireSimple.Backend.DomainModel.Analysis {
 			};
 		}
 
+		//TODO Move to Testing/Debugging
 		public static InvestmentModel testAnalysis2(StockInvestment investment, OptionsDict options) {
 			var value = investment.StockPrice * investment.StockQuantity;
 
@@ -51,5 +99,6 @@ namespace RetireSimple.Backend.DomainModel.Analysis {
 				}
 			};
 		}
+
 	}
 }
