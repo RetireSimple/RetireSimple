d52e45fa2fb726861e9ca4a6a9ac95d49a6bacd7
rhit-nikolira <70916208+rhit-nikolira@users.noreply.github.com>
2023-01-22 17:02:53 -0500

    Frontend visuals (#85)
    * front end visual chartJS
    
    * working front end
    
    * fixed CI unused imports

--- RetireSimple.Frontend/src/App.tsx
+++ RetireSimple.Frontend/src/App.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
-import { Investment } from './Models/Interfaces';
+import { Line } from 'react-chartjs-2';
+import { Investment, InvestmentModel } from './Models/Interfaces';
 
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
 
 export interface Forecast {
     date: string;
@@ -14,10 +25,70 @@ export interface Forecast {
 export default function App() {
     //const [forecasts, setForecasts] = React.useState<Forecast[]>([]);
     const [investments, setInvestments] = React.useState<Investment[]>([]);
-    //const [investmentModels, setInvestmentModels] = React.useState<InvestmentModel[]>([]);
+
+    const [investmentModelId, setInvestmentModelId] = React.useState("");
+
+    const [investmentModels, setInvestmentModels] = React.useState<InvestmentModel>();
+
     const [loading, setLoading] = React.useState<boolean>(true);
 
+        // const response = await fetch('/api/'); fetch data used in excel
+        // const data = await response.json();
+        ChartJS.register(
+            CategoryScale,
+            LinearScale,
+            PointElement,
+            LineElement,
+            Title,
+            Tooltip,
+            Legend
+        );
+       
+        const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+        const data = {
+            labels,
+            datasets: [
+                {
+                    label: 'Min Model',
+                    data: investmentModels?.minModelData,
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                },
+                {
+                    label: 'Avg Model',
+                    data: investmentModels?.avgModelData,
+                    borderColor: 'rgb(53, 162, 235)',
+                    backgroundColor: 'rgba(53, 162, 235, 0.5)',
+
+                },
+                {
+                    label: 'Max Model',
+                    data: investmentModels?.maxModelData,
+                    borderColor: 'rgb(53, 162, 235)',
+                    backgroundColor: 'rgba(53, 162, 235, 0.5)',
+},
+            ],
+        };
+        const options = {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top' as const,
+                },
+                title: {
+                    display: true,
+                    text: 'Chart.js Line Chart',
+                },
+            },
+        };
+        //return <Line options={options} data={data} />;
+
+    const getAnalysis = async () => {
+        const response = await fetch(`/api/Analysis/GetAnaylsis?investmentID=${investmentModelId}`, { method: 'POST' });
+        const data : InvestmentModel = await response.json();
+        setInvestmentModels(data);
+    }
 
     const populateInvestmentData = async () => {
         const response = await fetch('/api/Investment/GetAllInvestments');
@@ -36,6 +107,13 @@ export default function App() {
     React.useEffect(() => {
         if (loading) { populateInvestmentData() };
     });
+
+    const renderAnalysis = (sim: String, options: any, data: any) => {
+        switch (sim) {
+            case "MonteCarlo":
+                return <div> <Line options={options} data={data} /> </div>;
+        }
+    }
 
     const renderInvestmentsTable = (investments: Investment[]) => {
         return (
@@ -71,6 +149,8 @@ export default function App() {
     }
 
     {
+        let chart = renderAnalysis("MonteCarlo", options, data);
+
         let contents = loading
             ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
             : renderInvestmentsTable(investments);
@@ -78,10 +158,13 @@ export default function App() {
         return (
             <div>
                 <button onClick={addNewInvestment}>Add New Stonk</button>
+
                 <h1 id="tabelLabel" >Investments</h1>
                 <p></p>
+                <input type="text" onChange={e => setInvestmentModelId(e.target.value)}></input>
+                <button onClick={getAnalysis}>Get Analysis Model</button>
 
-
+                {chart}
                 {contents}
             </div>
 
