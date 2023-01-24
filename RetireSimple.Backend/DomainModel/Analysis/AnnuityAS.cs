8583a6fcd47b0cfe42997a84ada853b1add0cfbe
JinhaoSheng <shengj1@rose-hulman.edu>
2023-01-24 16:22:54 -0500

    Analysis option combiner (#95)
    * all methods in analysis created
    
    * test added

--- RetireSimple.Backend/DomainModel/Analysis/AnnuityAS.cs
+++ RetireSimple.Backend/DomainModel/Analysis/AnnuityAS.cs
@@ -1,5 +1,4 @@
-
-﻿using RetireSimple.Backend.DomainModel.Data;
+using RetireSimple.Backend.DomainModel.Data;
 using RetireSimple.Backend.DomainModel.Data.Investment;
 
 namespace RetireSimple.Backend.DomainModel.Analysis {
@@ -8,7 +7,26 @@ namespace RetireSimple.Backend.DomainModel.Analysis {
 		public static InvestmentModel DefaultAnnuityAnalyis(AnnuityInvestment investment, OptionsDict options) {
 			throw new NotImplementedException();
 		}
+
+		public static readonly OptionsDict DefaultAnnuityAnalysisOptions = new() {
+			["AnalysisLength"] = "60",                          //Number of months to project
+			["AnnuityAnalysisExpectedGrowth"] = "0.1",            //Expected Percentage Growth of the stock
+
+		};
+
+		public static OptionsDict AnnuityAnalysisOption(InvestmentBase investment, OptionsDict dict) {
+			var newDict = new OptionsDict(dict);
+			var investmentOptions = investment.AnalysisOptionsOverrides;
+
+			foreach(var k in investmentOptions.Keys) {
+				newDict.TryAdd(k, investmentOptions[k]);
+			}
+
+			foreach(var k in AnnuityAS.DefaultAnnuityAnalysisOptions.Keys) {
+				newDict.TryAdd(k, AnnuityAS.DefaultAnnuityAnalysisOptions[k]);
+			}
+
+			return newDict;
+		}
 	}
 }
-
-
--- RetireSimple.Backend/DomainModel/Analysis/BondAS.cs
+++ RetireSimple.Backend/DomainModel/Analysis/BondAS.cs
@@ -9,5 +9,26 @@ namespace RetireSimple.Backend.DomainModel.Analysis {
 			throw new NotImplementedException();
 		}
 
+		public static readonly OptionsDict DefaultBondAnalysisOptions = new() {
+			["AnalysisLength"] = "60",                          //Number of months to project
+			["BondAnalysisExpectedGrowth"] = "0.1",            //Expected Percentage Growth of the stock
+
+		};
+
+		public static OptionsDict BondAnalysisOption(InvestmentBase investment, OptionsDict dict) {
+			var newDict = new OptionsDict(dict);
+			var investmentOptions = investment.AnalysisOptionsOverrides;
+
+			foreach(var k in investmentOptions.Keys) {
+				newDict.TryAdd(k, investmentOptions[k]);
+			}
+
+			foreach(var k in BondAS.DefaultBondAnalysisOptions.Keys) {
+				newDict.TryAdd(k, BondAS.DefaultBondAnalysisOptions[k]);
+			}
+
+			return newDict;
+		}
+
 	}
 }
