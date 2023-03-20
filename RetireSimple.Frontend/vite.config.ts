a60d70fa34973bf360ff89e4d740ff83c57d68aa
Alex Westerman <westeraj@rose-hulman.edu>
2023-03-19 21:00:50 -0400

    Fix Build/Testing Issues

--- RetireSimple.Frontend/vite.config.ts
+++ RetireSimple.Frontend/vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+	plugins: [react(), viteTsconfigPaths()],
 	server: {
 		port: 3000,
 		open: '/',
