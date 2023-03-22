8df992212af3d7dc36fe726f78e1a79f81c698e2
Alex Westerman <westeraj@rose-hulman.edu>
2023-03-22 19:16:28 -0400

    Add some brief instructions on the homepage

--- RetireSimple.Frontend/src/routes/Root.tsx
+++ RetireSimple.Frontend/src/routes/Root.tsx
@@ -1,4 +1,4 @@
-import {Box, Button} from '@mui/material';
+import {Box, Button, Typography} from '@mui/material';
 import React from 'react';
 import {useNavigation} from 'react-router-dom';
 import {getAggregateModel} from '../api/InvestmentApi';
@@ -33,10 +33,33 @@ export const Root = () => {
 					height: '50%',
 					marginX: 'auto',
 					flexDirection: 'column',
+					objectFit: 'contain',
 				}}>
 				{!hasData && (
-					<Button onClick={getData} disabled={hasData} size='large'>
-						Get Data
+					<>
+						<Box>
+							<Typography variant='h6' sx={{flexGrow: 1, marginBottom: '0.5rem'}}>
+								Welcome to RetireSimple!
+							</Typography>
+							<Typography variant='body1' sx={{flexGrow: 1, marginBottom: '0.5rem'}}>
+								To get started, add some information about investments or vehicles
+								to the application.
+							</Typography>
+							<Typography variant='body1' sx={{flexGrow: 1, marginBottom: '0.5rem'}}>
+								Once you have added some information, you can view a portfolio
+								analysis by clicking the button below.
+							</Typography>
+							<Typography variant='body1' sx={{flexGrow: 1, marginBottom: '0.5rem'}}>
+								You can come back here using the "Home" item in the top of the
+								sidebar list.
+							</Typography>
+						</Box>
+					</>
+				)}
+
+				{!hasData && (
+					<Button onClick={getData} disabled={hasData} size='large' sx={{width: 'auto'}}>
+						Get Portfolio Analysis
 					</Button>
 				)}
 				{hasData && (
