014b67e48577ea5b7f2cd909a2796a37fcd82bed
rosenas <70673175+rosenas@users.noreply.github.com>
2023-12-20 15:24:29 -0500

    user

---
+++ RetireSimple.NewEngine/New_Engine/User/User.cs
@@ -0,0 +1,28 @@
+﻿using RetireSimple.NewEngine.New_Engine.Managers;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetireSimple.NewEngine.New_Engine.User {
+	public class User {
+
+
+		private UserInfo userInfo;
+
+		private Manager portfolioManager;
+
+		public User(UserInfo userInfo) {
+		
+			this.userInfo = userInfo;
+
+			this.portfolioManager = new PortfolioManager();
+		}
+
+
+
+	}
+}
---
+++ RetireSimple.NewEngine/New_Engine/User/UserInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetireSimple.NewEngine.New_Engine.User {
+	public class UserInfo {
+
+
+		public readonly DateOnly birthday;
+
+		public int retirementAge;
+
+		public long retirementGoal;
+
+
+		public UserInfo(DateOnly birthday, int retirementAge, long retirementGoal ) {
+			this.birthday = birthday;
+			this.retirementAge = retirementAge;
+			this.retirementGoal = retirementGoal;
+		}
+
+	}
+}
