fcbd3428f4af16054561b8052ce5569711855aec
rosenas <70673175+rosenas@users.noreply.github.com>
2023-12-26 14:17:57 -0600

    457b no testing

---
+++ RetireSimple.NewEngine/New_Engine/Financials/InvestmentVehicles/InvestmentVehicleInfos/_457bInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetireSimple.NewEngine.New_Engine.Financials.InvestmentVehicles.InvestmentVehicleInfos {
+	public class _457bInfo : InvestmentVehicleInfo {
+
+		//What percent of your salary do you plan on contributing yearly
+		public double contributions;
+
+		//How much money do you make a year
+		public double salary;
+
+		//How much do you expect for your salary to increase in a given year
+		public double salaryIncrease;
+
+		//What is your expected rate of return
+		public double rate;
+
+		//How much does your employer match
+		public double employerMatch;
+
+		//Up to what percent do they match until 
+		public double employerMatchCap;
+
+
+		public _457bInfo(double contributions, double salary, double salaryIncrease, double rate, double employerMatch, double employerMatchCap) {
+			this.contributions = contributions;
+			this.salary = salary;
+			this.salaryIncrease = salaryIncrease;
+			this.rate = rate;
+			this.employerMatch = employerMatch;
+			this.employerMatchCap = employerMatchCap;
+		}
+
+	}
+}
---
+++ RetireSimple.NewEngine/New_Engine/Financials/InvestmentVehicles/_457b/_457b.cs
@@ -0,0 +1,17 @@
+﻿using RetireSimple.NewEngine.New_Engine.Financials.InvestmentVehicles.InvestmentVehicleInfos;
+using RetireSimple.NewEngine.New_Engine.GrowthModels;
+using RetireSimple.NewEngine.New_Engine.GrowthModels._457bGrowthModels;
+using RetireSimple.NewEngine.New_Engine.TaxModels;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetireSimple.NewEngine.New_Engine.Financials.InvestmentVehicles._457b {
+	public class _457b : InvestmentVehicle {
+		public _457b(ITax tax, int id, float value, InvestmentVehicleInfo info) : base(tax, id, FinCategories.INVESTMENT_VEHICLE, value, new _457bGrowth() , info) {
+		}
+	}
+}
