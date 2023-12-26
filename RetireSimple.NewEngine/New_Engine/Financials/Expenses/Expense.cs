cd793bbeedd362032cd5d28dd73c8969e4513180
rosenas <70673175+rosenas@users.noreply.github.com>
2023-12-26 14:44:48 -0600

    starting expenses

---
+++ RetireSimple.NewEngine/New_Engine/Financials/Expenses/Expense.cs
@@ -0,0 +1,24 @@
+﻿using RetireSimple.Engine.New_Engine;
+using RetireSimple.NewEngine.New_Engine.TaxModels;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetireSimple.NewEngine.New_Engine.Financials.Expenses {
+	public abstract class Expense : Financial {
+		public Expense(ITax tax, int id ) : base(tax, id, FinCategories.EXPENSE) {
+
+
+
+		}
+
+		public override Projection Calculate(int years) {
+			return this.GenerateProjection(years);
+		}
+
+		public abstract Projection GenerateProjection(int years);
+	}
+}
---
+++ RetireSimple.NewEngine/New_Engine/Managers/ExpenseManager.cs
@@ -0,0 +1,23 @@
+﻿using RetireSimple.NewEngine.New_Engine.Financials;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetireSimple.NewEngine.New_Engine.Managers {
+	public class ExpenseManager : Manager {
+		public override bool Add(Financial f) {
+			if(f.category == FinCategories.EXPENSE) {
+				base.items.Add(f);
+				return true;
+			} else {
+				return false;
+			}
+		}
+		public override bool DoDelete(int id) => throw new NotImplementedException();
+		public override Financial DoRead(int id) => throw new NotImplementedException();
+		public override bool DoUpdate(Financial f, int id) => throw new NotImplementedException();
+	}
+}
