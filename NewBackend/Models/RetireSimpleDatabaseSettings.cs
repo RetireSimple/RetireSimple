5d18e147f16fa2f62d5ed958b7082c9c8fe98164
rhit-wighths <wighths@rose-hulman.edu>
2024-01-18 17:57:50 -0500

    new backend started

---
+++ NewBackend/Models/RetireSimpleDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace NewBackend.Models {
+	public class RetireSimpleDatabaseSettings {
+
+		public string ConnectionString { get; set; }
+
+		public string DatabaseName { get; set; } = null!;
+
+		public string UsersCollectionName { get; set; } = null!;
+
+
+	}
+}
